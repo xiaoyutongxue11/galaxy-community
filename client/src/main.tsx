9496877ed9ceb39d5b6aa10d26e7c0e8ad00980b
tyy123 <1572235056@qq.com>
2024-09-10 16:18:07 +0800

    feat：创建界面路由

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { App, ConfigProvider } from "antd";
+import { BrowserRouter } from "react-router-dom";
+import zhCN from "antd/es/locale/zh_CN";
+import RouteRender from "./router";
+import "@/assets/styles/global.less";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#a971e5",
+        },
+      }}
+      locale={zhCN}
+    >
+      <App>
+        <RouteRender />
+      </App>
+    </ConfigProvider>
+  </BrowserRouter>
 );
---
+++ client/src/router/config.ts
@@ -0,0 +1,22 @@
+// import { lazy } from "react";
+import Login from "../components/login";
+import Register from "@/components/register";
+
+export interface IRouter {
+  name?: string;
+  redirect?: string;
+  path: string;
+  children?: Array<IRouter>;
+  component: React.ComponentType;
+}
+
+export const router: Array<IRouter> = [
+  {
+    path: "/login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    component: Register,
+  },
+];

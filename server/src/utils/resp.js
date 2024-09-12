823bcec889bbff542de284fcffcbedc8aaf9fdda
tyy123 <1572235056@qq.com>
2024-09-12 22:31:18 +0800

    feat：封装响应体

---
+++ server/src/utils/resp.js
@@ -0,0 +1,37 @@
+// 响应数据
+const { ErrStatusMap } = require('./error');
+
+const respHttp = (res, respCode, data) => {
+  const resp = {
+    code: 200,
+    data: '',
+    message: 'success'
+  };
+
+  if (respCode !== 200) {
+    resp.code = respCode;
+    resp.message = ErrStatusMap[respCode];
+  } else resp.data = data;
+  res.json(resp);
+};
+
+// 请求成功
+const RespSuccess = res => {
+  respHttp(res, 200);
+};
+
+// 请求失败
+const RespError = (res, respCode) => {
+  respHttp(res, respCode);
+};
+
+// 请求成功且响应数据
+const RespData = (res, data) => {
+  respHttp(res, 200, data);
+};
+
+module.exports = {
+  RespSuccess,
+  RespError,
+  RespData
+};

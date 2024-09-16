d74d75bdb689693210e2869ee171314db22bb68a
tyy123 <1572235056@qq.com>
2024-09-16 22:08:16 +0800

    chore：sessionStorage封装

---
+++ client/src/utils/storage.ts
@@ -0,0 +1,50 @@
+export const sessionStorageKey = 'Galaxy-Community.';
+
+interface ISessionStorage<T> {
+  key: string;
+  defaultValue: T;
+}
+
+// 重新封装 sessionStorage
+export class Storage<T> implements ISessionStorage<T> {
+  key: string;
+  defaultValue: T;
+
+  constructor(key: string, defaultValue: T) {
+    this.key = sessionStorageKey + key;
+    this.defaultValue = defaultValue;
+  }
+  setItem(value: T) {
+    sessionStorage.setItem(this.key, JSON.stringify(value));
+  }
+  getItem(): T {
+    const value = sessionStorage[this.key] && sessionStorage.getItem(this.key);
+    if (value === undefined) return this.defaultValue;
+    try {
+      return value && value !== 'null' && value !== 'undefined'
+        ? (JSON.parse(value) as T)
+        : this.defaultValue;
+    } catch {
+      return value && value !== 'null' && value !== 'undefined'
+        ? (value as unknown as T)
+        : this.defaultValue;
+    }
+  }
+  removeItem() {
+    sessionStorage.removeItem(this.key);
+  }
+}
+
+/** 管理 token 的sessionStorage */
+export const tokenStorage = new Storage<string>('authToken', '');
+/** 管理用户信息的sessionStorage */
+export const userStorage = new Storage<string>('userInfo', '');
+
+/** 只清除当前项目所属的本地存储 */
+export const clearSessionStorage = () => {
+  for (const key in sessionStorage) {
+    if (key.includes(sessionStorageKey)) {
+      sessionStorage.removeItem(key);
+    }
+  }
+};

02c27118ab4b9742f7f27dba375ae092e14992e0
tyy123 <1572235056@qq.com>
2024-09-29 18:32:57 +0800

    style：左侧列表栏

--- client/src/pages/container/components/chat/index.tsx
+++ client/src/pages/container/components/chat/index.tsx
@@ -16,41 +16,20 @@ const Chat = () => {
           <PlusCircleOutlined style={{ fontSize: '1rem' }} />
         </div>
         <div className={styles.list}>
-          <div className={styles.item}>
-            <img src={Logo} style={{ width: '2rem', height: '2rem' }} />
-            <div className={styles.info}>
-              <p style={{ fontWeight: 'bold' }}>xiaoyutongxue</p>
-              <p style={{ fontSize: '10px', color: '#aaa' }}>今天很开心</p>
+          {Array.from({ length: 15 }, (_, index) => (
+            <div className={styles.listItem} key={index}>
+              <img src={Logo} />
+              <div className={styles.info}>
+                <div className={styles.infoTop}>
+                  <p>xiaoyutongxuehahahhahahahahhaaaa</p>
+                  <span>2024/9/8</span>
+                </div>
+                <p className={styles.infoBottom}>
+                  今天复合地基刷个卡世界观和手机阿嘎户籍卡三打哈估计gfsgg
+                </p>
+              </div>
             </div>
-          </div>
-          <div className={styles.item}>
-            <img src={Logo} style={{ width: '2rem', height: '2rem' }} />
-            <div className={styles.info}>
-              <p style={{ fontWeight: 'bold' }}>xiaoyutongxue</p>
-              <p style={{ fontSize: '10px', color: '#aaa' }}>今天很开心</p>
-            </div>
-          </div>
-          <div className={styles.item}>
-            <img src={Logo} style={{ width: '2rem', height: '2rem' }} />
-            <div className={styles.info}>
-              <p style={{ fontWeight: 'bold' }}>xiaoyutongxue</p>
-              <p style={{ fontSize: '10px', color: '#aaa' }}>今天很开心</p>
-            </div>
-          </div>
-          <div className={styles.item}>
-            <img src={Logo} style={{ width: '2rem', height: '2rem' }} />
-            <div className={styles.info}>
-              <p style={{ fontWeight: 'bold' }}>xiaoyutongxue</p>
-              <p style={{ fontSize: '10px', color: '#aaa' }}>今天很开心</p>
-            </div>
-          </div>
-          <div className={styles.item}>
-            <img src={Logo} style={{ width: '2rem', height: '2rem' }} />
-            <div className={styles.info}>
-              <p style={{ fontWeight: 'bold' }}>xiaoyutongxue</p>
-              <p style={{ fontSize: '10px', color: '#aaa' }}>今天很开心</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <div className={styles.center}>chat</div>
